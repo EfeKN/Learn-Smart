a2f116925c12ad8d51649d366f18c714e1503bd7
murat-cagri <cagri.kara@ug.bilkent.edu.tr>
2024-06-30 14:52:36 +0300

    Frontend pages are updated and styled.

--- frontend/app/create-account/page.tsx
+++ frontend/app/create-account/page.tsx
@@ -3,18 +3,18 @@
 import backendAPI from "@/environment/backend_api";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { FaRegEnvelope, FaRegUser } from "react-icons/fa";
+import { MdLockOutline } from "react-icons/md";
+import { RiShieldUserLine } from "react-icons/ri"
 
 const CreateAccountPage: React.FC = () => {
   const [name, setName] = useState<string>(""); // State for name input
+  const [email, setEmail] = useState<string>(""); // State for name input
   const [password, setPassword] = useState<string>(""); // State for password input
-
-  // Next.js router hook
   const router = useRouter();
 
-  // Function to handle account creation
   // Sends a POST request to the backend to create a new user account
   // Redirects to login page after successful account creation
-  //
   const handleCreateAccount = async () => {
     try {
       // Send a POST request to the backend to create a new user account
@@ -35,39 +35,78 @@ const CreateAccountPage: React.FC = () => {
   };
 
   return (
-    <div className="flex flex-col items-center mt-1">
-      <h1>Create Account</h1>
-
-      <div className="mb-1">
-        <label>
-          Name:
-          <input
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            className="ml-1"
-          />
-        </label>
+    <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center bg-gray-100 min-h-screen">
+      <div className="bg-white text-black rounded-2xl shadow-2xl flex w-2/3 max-w-4xl">
+        <div className="w-3/5 p-5">
+          <div className="text-left font-bold mb-10">
+            <span className="text-black-500">Learn</span>Smart
+          </div>
+          <div className="py-5">
+            <h2 className="text-3xl font-bold text-black-500 mb-2">Create Account</h2>
+          </div>
+          <div className="border-2 w-10 border-black inline-block mb-2"></div>
+          <div className="flex flex-col items-center">
+            <div className="bg-gray-100 w-64 p-2 flex items-center mb-2">
+              <FaRegEnvelope/>
+              <input
+                  type="text"
+                  value={email}
+                  placeholder="Email"
+                  onChange={(e) => setName(e.target.value)}
+                  className="ml-1 bg-gray-100 outline-none text-sm flex-1"
+              />
+            </div>
+            <div className="bg-gray-100 w-64 p-2 flex items-center mb-2">
+              <FaRegUser/>
+              <input
+                  type="text"
+                  value={name}
+                  placeholder="Name"
+                  onChange={(e) => setName(e.target.value)}
+                  className="ml-1 bg-gray-100 outline-none text-sm flex-1"
+              />
+            </div>
+            <div className="bg-gray-100 w-64 p-2 flex items-center mb-2">
+              <RiShieldUserLine/>
+              <input
+                  type="text"
+                  value={name}
+                  placeholder="Username"
+                  onChange={(e) => setName(e.target.value)}
+                  className="ml-1 bg-gray-100 outline-none text-sm flex-1"
+              />
+            </div>
+            <div className="bg-gray-100 w-64 p-2 flex items-center mb-5">
+              <MdLockOutline/>
+              <input
+                  type="password"
+                  value={password}
+                  placeholder="Password"
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="ml-1 bg-gray-100 outline-none text-sm flex-1"
+              />
+            </div>
+          </div>
+          <button
+              onClick={handleCreateAccount}
+              className="border-2 bg-white border-black text-black rounded-full px-12 py-2 inline-block font-semibold hover:bg-black hover:text-white"
+          >
+            Create Account
+          </button>
+        </div>
+        <div
+            className="w-2/5 bg-black text-white rounded-tr-2xl rounded-br-2xl flex flex-col items-center justify-center py-28 px-12">
+          <h2 className="text-3xl font-bold mb-10">Log In</h2>
+          <div className="border-2 w-10 border-white inline-block mb-32"></div>
+          <button
+              onClick={() => router.push("/login")}
+              className="border-2 text-white border-white bg-black rounded-full px-12 py-2 inline-block font-semibold hover:bg-white hover:text-black"
+          >
+            Back to Login
+          </button>
+        </div>
       </div>
-
-      <div className="mb-1">
-        <label>
-          Password:
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="ml-1"
-          />
-        </label>
-      </div>
-
-      <button onClick={handleCreateAccount} className="mb-1">
-        Create Account
-      </button>
-
-      <button onClick={() => router.push("/login")}>Back to Login</button>
-    </div>
+    </main>
   );
 };
 
--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -3,6 +3,8 @@
 import backendAPI from "@/environment/backend_api";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import {FaRegEnvelope} from "react-icons/fa";
+import {MdLockOutline} from "react-icons/md";
 
 const LoginPage: React.FC = () => {
   const [name, setName] = useState<string>("");
@@ -29,36 +31,65 @@ const LoginPage: React.FC = () => {
   };
 
   return (
-    <div className="flex flex-col items-center mt-10">
-      <h1>Login</h1>
-      <div className="mb-2">
-        <label>
-          Name:
-          <input
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            className="ml-2"
-          />
-        </label>
-      </div>
-      <div className="mb-2">
-        <label>
-          Password:
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="ml-2"
-          />
-        </label>
-      </div>
-      <button onClick={handleLogin} className="mb-2">
-        Login
-      </button>
-      <button onClick={handleCreateAccount}>Create Account</button>
-    </div>
-  );
-};
+    <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center bg-gray-100 min-h-screen">
+        <div className="bg-white text-black rounded-2xl shadow-2xl flex w-2/3 max-w-4xl">
+            <div className="w-3/5 p-5">
+                <div className="text-left font-bold mb-10">
+                    <span className="text-black-500">Learn</span>Smart
+                </div>
+                <div className="py-5">
+                    <h2 className="text-3xl font-bold text-black-500 mb-2">Login to LearnSmart</h2>
+                </div>
+                <div className="border-2 w-10 border-black inline-block mb-2"></div>
+                <div className="flex flex-col items-center">
+                    <div className="bg-gray-100 w-64 p-2 flex items-center mb-3">
+                        <FaRegEnvelope/>
+                        <input
+                            type="text"
+                            value={name}
+                            placeholder="Email or Username"
+                            onChange={(e) => setName(e.target.value)}
+                            className="ml-1 bg-gray-100 outline-none text-sm flex-1"
+                        />
+                    </div>
+                    <div className="bg-gray-100 w-64 p-2 flex items-center mb-3">
+                        <MdLockOutline/>
+                        <input
+                            type="password"
+                            value={password}
+                            placeholder="Password"
+                            onChange={(e) => setPassword(e.target.value)}
+                            className="ml-1 bg-gray-100 outline-none text-sm flex-1"
+                        />
+                    </div>
+                </div>
+                <div className="flex justify-between w-64 mb-5 mx-auto">
+                    <label className="flex items-center text-xs">
+                        <input type="checkbox" name="remember" className="mr-1"/>
+                        Remember Me
+                    </label>
+                    <a href="#" className="text-xs">Forgot Password?</a>
+                </div>
+                <button
+                    onClick={handleLogin}
+                    className="border-2 bg-white border-black text-black rounded-full px-12 py-2 inline-block font-semibold hover:bg-black hover:text-white"
+                >
+                    Login
+                </button>
+            </div>
+            <div
+                className="w-2/5 bg-black text-white rounded-tr-2xl rounded-br-2xl flex flex-col items-center justify-center py-28 px-12">
+                <h2 className="text-3xl font-bold mb-10">Create Account</h2>
+                <div className="border-2 w-10 border-white inline-block mb-32"></div>
+                <button
+                    onClick={handleCreateAccount}
+                    className="border-2 text-white border-white bg-black rounded-full px-12 py-2 inline-block font-semibold hover:bg-white hover:text-black"
+                >
+                    Create Account
+                </button>
+            </div>
+        </div>
+    </main>
+  )};
 
 export default LoginPage;
