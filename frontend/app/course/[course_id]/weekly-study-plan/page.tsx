9966084d1adcd830f3850870ef1711708dce35df
EfeKN <efe.kaan.fidanci@gmail.com>
2024-07-18 01:21:28 +0300

    temporary fix for weekly-study-plan .md file path

--- frontend/app/course/[course_id]/weekly-study-plan/page.tsx
+++ frontend/app/course/[course_id]/weekly-study-plan/page.tsx
@@ -4,6 +4,7 @@ import "@/app/style/course-homepage.css";
 import backendAPI from "@/environment/backend_api";
 import Cookies from "js-cookie";
 import Markdown from "../../../components/markdown-renderer"
+import axios from "axios";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
@@ -33,11 +34,29 @@ export default function StudyPlan() {
       });
   
       const studyPlanUrl = response.data.study_plan_url;
-      console.log(studyPlanUrl); // TODO:
-                                 // => This gives the location: http://localhost:8000/api/files/course_2/study_plan.md
-                                 // When api is removed from the path, the component works as expected 
-  
-      const studyPlanResponse = await backendAPI.get("http://localhost:8000/files/course_2/study_plan.md", {
+      
+      // TODO:
+
+      /*
+
+        await backendAPI.get(`${studyPlanUrl}`, {
+        headers: {
+          Accept: "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        
+              // => This gives the location: http://localhost:8000/api/files/course_2/study_plan.md
+             // When api is removed from the path, the component works as expected
+        
+      */
+        
+      // ========================= TEMPORARY FIX =========================
+      const temp_backendAPI = axios.create({
+        baseURL: 'http://localhost:8000/',
+      })
+      // ========================= TEMPORARY FIX =========================
+
+      const studyPlanResponse = await temp_backendAPI.get(`${studyPlanUrl}`, {
         headers: {
           Accept: "application/json",
           Authorization: `Bearer ${token}`,
